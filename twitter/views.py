f2dd2e95bc1321a5f6401ac99d8cfa08a8ff71ec
vasya <vaniat207@mail.ru>
2014-12-12 20:44:42 +0200

    second

--- twitter/views.py
+++ twitter/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render_to_response
 from twitter.models import Twit
 def index(request):
+    print request.GET
+    if 'q' in request.GET:
+        v = Twit()
+        v.text = request.GET['q']
+        v.save()
 
-    twits=Twit.objects.all
+    twits=Twit.objects.all()
     return render_to_response("twitter/index.html",{'twits':twits})
 # Create your views here.
